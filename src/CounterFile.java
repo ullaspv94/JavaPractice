619b7bcc73a23d4056207224c397498c41fa4232
Ashutosh Pandey <ashutosh.pandey@gmail.com>
2018-09-09 18:01:03 +0530

    removed github issue template

---
+++ src/CounterFile.java
@@ -0,0 +1,11 @@
+/*
+ *  Program to count the number of character, no of word, no of lines in a given file
+ *  (Assignment 5, Problem 1)
+ *  --------------------------------------------------------------------------------
+ *  We'll solve this using the following two approaches:
+ *   (1) Using string handling functionality
+ *   (2) Building own logic to count
+ */
+public class CounterFile {
+
+}
